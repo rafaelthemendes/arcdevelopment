593381c23887747971ed9c2adb5577170cb1647e
Rafael Mendes <rafaelthemendes@gmail.com>
2021-12-31 09:46:33 -0300

    Playing a bit with Typography and inline styles

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -13,12 +19,22 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: { ...theme.mixins.toolbar },
+}));
+
 export default function Header() {
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <AppBar position="fixed">
-        <Toolbar>Arc Development</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h3">Arc Development</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 }
