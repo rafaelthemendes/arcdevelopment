996a6cc3d543c2ee8787273b3cd4a7d3f3a56b6e
Rafael Mendes <rafaelthemendes@gmail.com>
2021-12-30 21:12:32 -0300

    Removed scrollable text and useless config in the ElevationScroll

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,6 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      {[...new Array(300)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,14 +2,10 @@ import React from "react";
 import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
 
 function ElevationScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
+  const { children } = props;
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
-    target: window ? window() : undefined,
   });
 
   return React.cloneElement(children, {
