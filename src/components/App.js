3663a681f02574afc184f7ae97a65ce9eecd5268
Rafael Mendes <rafaelthemendes@gmail.com>
2021-12-30 21:11:24 -0300

    Added material ui styles and created a Theme with some custom colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import Header from "./ui/Header";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./ui/Theme";
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
       {[...new Array(300)]
         .map(
@@ -12,7 +14,7 @@ Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
         )
         .join("\n")}
-    </div>
+    </ThemeProvider>
   );
 }
 
