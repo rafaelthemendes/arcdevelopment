d7885392b8915ce35bb1a97cfa51509819d3f8ac
Rafael Mendes <rafael.mendes@x-team.com>
2022-01-16 16:26:05 -0300

    Landing Page - Custom Software Development block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,8 @@
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Button, Grid, Typography, useMediaQuery } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Lottie from "react-lottie";
-import animationData from "../animations/landinganimation/data";
+import animationData from "../animations/landingAnimation/data";
+import customSoftwareIcon from "../assets/customSoftwareIcon.svg";
 import ButtonArrow from "./ui/ButtonArrow";
 
 const useStyles = makeStyles((theme) => ({
@@ -45,22 +46,45 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderRadius: 50,
-    borderWidth: 2,
-    textTransform: "none",
-    fontFamily: "Roboto",
-    fontWeight: "bold",
+    ...theme.typography.learnButton,
     fontSize: "0.9rem",
     height: 45,
     width: 145,
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 8,
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em",
+    },
+  },
+  specialText: {
+    fontFamily: "Pacifico",
+    color: theme.palette.common.orange,
+  },
+  subtitle: {
+    marginBottom: "1em",
+  },
+  icon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "7em",
+    },
   },
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
+  const screenDownSm = useMediaQuery(theme.breakpoints.down("sm"));
 
   const defaultOptions = {
     loop: true,
@@ -71,48 +95,96 @@ export default function LandingPage() {
     },
   };
 
-  return (
-    <Grid container className={classes.root} direction="column">
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-          justifyContent="flex-end"
-          alignItems="center"
-        >
-          <Grid item sm className={classes.heroTextContainer}>
-            <Typography variant="h2" align="center">
-              Bringing West Coast Technology
-              <br />
-              to the Midwest
-            </Typography>
-            <Grid
-              container
-              className={classes.buttonContainer}
-              justifyContent="center"
-            >
-              <Grid item>
-                <Button className={classes.estimateButton} variant="contained">
-                  Free Estimate
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button className={classes.learnButtonHero} variant="outlined">
-                  <span style={{ marginRight: 10 }}>Learn More</span>
-                  <ButtonArrow
-                    width={15}
-                    height={15}
-                    fill={theme.palette.common.blue}
-                  />
-                </Button>
-              </Grid>
+  const heroBlock = (
+    <Grid item>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+      >
+        <Grid item sm className={classes.heroTextContainer}>
+          <Typography variant="h2" align="center">
+            Bringing West Coast Technology
+            <br />
+            to the Midwest
+          </Typography>
+          <Grid
+            container
+            className={classes.buttonContainer}
+            justifyContent="center"
+          >
+            <Grid item>
+              <Button className={classes.estimateButton} variant="contained">
+                Free Estimate
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button className={classes.learnButtonHero} variant="outlined">
+                <span style={{ marginRight: 10 }}>Learn More</span>
+                <ButtonArrow
+                  width={15}
+                  height={15}
+                  fill={theme.palette.common.blue}
+                />
+              </Button>
             </Grid>
           </Grid>
-          <Grid item sm className={classes.animation}>
-            <Lottie options={defaultOptions} width="100%" height="100%" />
-          </Grid>
+        </Grid>
+        <Grid item sm className={classes.animation}>
+          <Lottie options={defaultOptions} width="100%" height="100%" />
         </Grid>
       </Grid>
+    </Grid>
+  );
+
+  const servicesBlock = (
+    <Grid item>
+      <Grid
+        container
+        direction="row"
+        justifyContent={screenDownSm ? "center" : undefined}
+        className={classes.serviceContainer}
+      >
+        <Grid
+          item
+          style={{
+            marginLeft: screenDownSm ? 0 : "5em",
+            textAlign: screenDownSm ? "center" : undefined,
+          }}
+        >
+          <Typography variant="h4">Custom Software Development</Typography>
+          <Typography variant="subtitle1" className={classes.subtitle}>
+            Save Energy. Save Time. Save Money.
+          </Typography>
+          <Typography variant="subtitle1">
+            Complete digital solutions, from investigation to{" "}
+            <span className={classes.specialText}>celebration.</span>
+          </Typography>
+          <Button variant="outlined" className={classes.learnButton}>
+            <span style={{ marginRight: 10 }}>Learn More</span>
+            <ButtonArrow
+              width={10}
+              height={10}
+              fill={theme.palette.common.blue}
+            />
+          </Button>
+        </Grid>
+        <Grid item>
+          <img
+            alt="custom software icon"
+            src={customSoftwareIcon}
+            className={classes.icon}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+
+  return (
+    <Grid container className={classes.root} direction="column">
+      {heroBlock}
+      {servicesBlock}
     </Grid>
   );
 }
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,6 +2,7 @@ import { createTheme } from "@material-ui/core/styles";
 
 const arcBlue = "#0B72B9";
 const arcOrange = "#FFBA60";
+const arcGrey = "#868686";
 
 export default createTheme({
   palette: {
@@ -29,12 +30,32 @@ export default createTheme({
       textTransform: "none",
       color: "white",
     },
+    learnButton: {
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderRadius: 50,
+      borderWidth: 2,
+      textTransform: "none",
+      fontFamily: "Roboto",
+      fontWeight: "bold",
+    },
     h2: {
       fontFamily: "Raleway",
-      fontWeight: 700,
       fontSize: "2.5rem",
+      fontWeight: 700,
       lineHeight: 1.5,
       color: arcBlue,
+    },
+    h4: {
+      fontFamily: "Raleway",
+      fontSize: "1.75rem",
+      fontWeight: 700,
+      color: arcBlue,
+    },
+    subtitle1: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: arcGrey,
     },
   },
 });
